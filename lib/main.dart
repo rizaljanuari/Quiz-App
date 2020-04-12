0e8cdfcb88d8ee48d45713f2a76d4505997b1c2a
rizaljanuari <rizaljanuari4@gmail.com>
2020-04-13 00:50:45 +0700

    adding ternary operation (?) and (:) on Question column

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex++;
-    });
-
-    // print(_questionIndex);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
+  final questions = const [
       //its called Maps
       {
         "questionText": "What's your favorite food?",
@@ -37,23 +25,45 @@ class _MyAppState extends State<MyApp> {
         "answer": ["Kentang Goreng", "Chiki Balls", "Taro", "Tahu bulat"]
       }
     ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+      if (_questionIndex < questions.length) {
+        print('we have more questions');  
+      } else {
+        print('we have no more');
+      }
+    });
+
+    // print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex]["questionText"],
-            ),
-            //the three dots (...) are Dart operator syntax called Spread Operator. Its mean they take a list and pull all the values in the list out of it andd add them to the surrounding list as individual values.
-            ...(questions[_questionIndex]["answer"] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questions[_questionIndex]["questionText"],
+                  ),
+                  //the three dots (...) are Dart operator syntax called Spread Operator. Its mean they take a list and pull all the values in the list out of it andd add them to the surrounding list as individual values.
+                  ...(questions[_questionIndex]["answer"] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text("Quiz finished!"),
+              ),
       ),
     );
   }
